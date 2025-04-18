d012fd25a195c2c35e19fe40cb9b16b75158d241
Your Name <your.email@example.com>
2025-04-19 01:29:28 +0530

    eight

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from . import db
-from .models import User
+from .models import User, Category
 import re
 from datetime import datetime, timedelta
 from .forms import SignUpForm, LoginForm
